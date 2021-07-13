7dfc5ba2a4cb2b96e973dbfed153528e28dd987e
Yang Zhou <deedecho@gmail.com>
2021-07-13 23:34:04 +0800

    add docs

--- src/index.js
+++ src/index.js
@@ -17,6 +17,27 @@ async function main () {
   ]);
 
   console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+
+  // Get all borrowers by scanning the AccountBorrows of each active market. 
+  // Perform every 5 minutes asynchronously.
+
+  // Get the (liquidity, shortfall) for each borrower, and put the borrower who has a 
+  // positive shortfall into the liquidation message queue. Perform every 5 minutes asynchronously.
+  // Message queue can adopt redis or postgreSQL.
+
+  // Get borrower from message queue, and get the latest (liquidity, shortfall) for the borrower.
+  
+  // Scan all the debit asset of the borrower, and sort the value of borrow balance in descending order.
+  // The top asset is the best liquidation token.
+
+  // Scan all the collateral asset of the borrower, and sort the value of collateral in descending order.
+  // The top asset is the best collateral token.
+
+  // Assume that A is the liquidation token, B is the collateral token
+  // Calculate the repay amount. 
+  // repayAmount = min(liquidator's balance of A, closeFactor * A's borrow balance of borrower, The total value of B(borrower's) / B's price)
+
+  // Liquidate borrow. 
 }
 
 main().catch(console.error).finally(() => process.exit());
