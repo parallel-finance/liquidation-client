2beec44959d3ba594210b0d94c6ce8d2cecbee39
DDDYuan <yuandi93@hotmail.com>
2022-04-17 18:50:30 +0800

    change to use SADD command only once

--- src/scan/refreshBorrowersRedis.ts
+++ src/scan/refreshBorrowersRedis.ts
@@ -1,12 +1,16 @@
 import { RedisClient } from '../types';
 import { logger } from '../logger';
 
+const SEY_KEY = 'borrowers';
+
 const refreshBorrowersRedis =
   (client: RedisClient) =>
   async (borrowers: string[]): Promise<void> => {
     logger.debug(`REDIS:got ${borrowers.length} underwater accounts to refresh.`);
-    const transaction = client.multi().del('borrowers');
-    borrowers.forEach((address) => transaction.sAdd('borrowers', address));
+    const transaction = client.multi().del(SEY_KEY);
+    if (borrowers.length > 0) {
+      transaction.sAdd(SEY_KEY, borrowers);
+    }
     logger.debug(`REDIS:start refresh transaction.`);
     await transaction.exec();
     logger.debug(`REDIS:refresh done.`);
